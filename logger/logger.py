a83a8f8c745f9135b171e9bebdb9656241f0020b
JCloete <jasonscloete@gmail.com>
2019-10-12 15:24:18 +0200

    forgot to change pin num

--- logger/logger.py
+++ logger/logger.py
@@ -36,11 +36,11 @@ def init_pi():
 
     # set up frequency switch button
     wiringpi.pinMode(2, wiringpi.INPUT)
-    wiringpi.pullUpDnControl(0, wiringpi.PUD_UP)
+    wiringpi.pullUpDnControl(2, wiringpi.PUD_UP)
 
     # set up reset button
     wiringpi.pinMode(3, wiringpi.INPUT)
-    wiringpi.pullUpDnControl(0, wiringpi.PUD_UP)
+    wiringpi.pullUpDnControl(3, wiringpi.PUD_UP)
 
     # init interrupts
     wiringpi.wiringPiISR(0, wiringpi.INT_EDGE_FALLING, monitoring) # stop/start
